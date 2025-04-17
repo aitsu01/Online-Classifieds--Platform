c04d65419004a780a97323fb0667074365d956d0
Fabrizio Pavone <fabriziopavone9985@gmail.com>
2025-04-17 17:56:50 +0200

    aggiunto form create livewire

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function create() {
-        return view('article.create');
+        $categories = Category::all();
+        return view('article.create', compact('categories'));
     }
     
 }
