48131f07cbd48ee022a1f68ad18a229afcf067d0
Danilo Amoruso <daniloamorusoad@gmail.com>
2025-04-18 16:57:34 +0200

    User Story 1 completata

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,11 +30,17 @@
       
         <div class="row mt-5">
 
-            <div class="col-12">
+            <div class="col-12 my-5">
                 <h1 class="text-center secondario">Presto project</h1>
-                
             </div>
         </div>
+        @auth
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-1">
+                <a class="btn btn-primary" href="{{route('article.create')}}">Pubblica un articolo</a>
+            </div>
+        </div>
+        @endauth
 
         
 
