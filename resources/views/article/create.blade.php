3326b3cea4fe401c1e03353448c65efba836968d
Danilo Amoruso <daniloamorusoad@gmail.com>
2025-04-18 16:34:06 +0200

    modifica

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -8,7 +8,7 @@
         </div>
         <div class="row justify-content-center mt-3">
             <div class="col-12 col-md-6">
-                <livewire:create-article-form />
+                <livewire:create-article-form/>
             </div>
         </div>        
     </div>
