006365ae1df884a3a675d0141ade7b1b8da63c9e
Anna Vincenzi <vincenzi.anna.41@gmail.com>
2025-04-22 12:31:46 +0200

    ottimizzato stile generale

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -34,7 +34,3 @@
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> 
-<<<<<<< HEAD
-=======
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script> 
->>>>>>> c38171de1349a0c5a290eeff7d4bca7b330443f4
