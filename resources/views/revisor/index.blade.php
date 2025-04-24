cf4fb3f5afb8c2cadaa641862767043a5521f1ad
Fabrizio Pavone <fabriziopavone9985@gmail.com>
2025-04-24 11:43:33 +0200

    modificato stile

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -52,7 +52,7 @@
             <h1 class="fst-italic display-4">
                 Nessun articolo da revisionare
             </h1>
-            <a href="{{route('homepage')}}" class="mt-5 btn btn-success">Torna alla homepage</a>
+            <a href="{{route('homepage')}}" class="mt-5 btn btn-custom1">Torna alla homepage</a>
         </div>
     </div>
     @endif
