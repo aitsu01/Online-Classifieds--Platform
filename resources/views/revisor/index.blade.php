91279a1fd419110bad5568e0654d02706f003d7e
Danilo Amoruso <daniloamorusoad@gmail.com>
2025-05-05 14:45:00 +0200

    aggiunto stile pagina revisore

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -1,10 +1,10 @@
 <x-layout>
-
+    
     <div class="row mt-5">
         <div class="col-1 mt-5">
             <a href="{{route('homepage')}}"><i class="bi bi-arrow-left fs-2 fw-bold"></i></a>
         </div>
-      </div>
+    </div>
     
     <div class="row mt-5">
         <div class="col-12 mt-5">
@@ -17,21 +17,22 @@
             </form>
         </div>
     </div>
-        
+    
     @if (session()->has('message'))
-        <div class="row justify-content-center align-items-center">
-            <div class="col-4 text-center shadow rounded alert alert-success fs-5 font1">
-                {{session('message')}}
-            </div>
+    <div class="row justify-content-center align-items-center">
+        <div class="col-4 text-center shadow rounded alert alert-success fs-5 font1">
+            {{session('message')}}
         </div>
+    </div>
     @endif
     
     @if ($article_to_check)
-    <div class="row justify-content-center pt-5">
-        <div class="col-md-6">
+    <div class="row justify-content-evenly pt-5">
+        <div class="col-md-8">
             <div class="row justify-content-center">
                 @if($article_to_check->images->isNotEmpty())
-                    @foreach ($article_to_check->images as $key => $image)
+                @foreach ($article_to_check->images as $key => $image)
+                <div class="bord rounded shadow mb-4 d-flex p-2">
                     <div class="col-4 col-md-3 mb-4 mx-2 p-0">
                         <img src="{{$image->getUrl(300,300)}}" class="img-fluid w-75 rounded shadow" alt="Immagine {{$key + 1}} dell'articolo {{$article_to_check->title}}">
                     </div>
@@ -39,15 +40,15 @@
                         <div class="card-body">
                             <h5>Labels</h5>
                             @if ($image->labels)
-                                    @foreach ($image->labels as $label)
-                                        #{{$label}},
-                                    @endforeach
-                                @else
-                                    <p class="fst-italic">No Lables</p>
+                            @foreach ($image->labels as $label)
+                            #{{$label}},
+                            @endforeach
+                            @else
+                            <p class="fst-italic">No Lables</p>
                             @endif
                         </div>
                     </div>
-                    <div class="col-md-8 ps-3">
+                    <div class="col-md-3 ps-3">
                         <div class="card-body">
                             <h5>Ratings</h5>
                             <div class="row justify-content-center">
@@ -87,13 +88,14 @@
                             </div>
                         </div>
                     </div>
-                    @endforeach
+                </div>
+                @endforeach
                 @else
-                    <img src="/media/placeholder.jpg" alt="" class="img-fluid w-50 rounded shadow">
+                <img src="/media/placeholder.jpg" alt="" class="img-fluid w-50 rounded shadow">
                 @endif
             </div>
         </div>
-        <div class="col-md-5 col-8 p-0 d-flex flex-column justify-content-between">
+        <div class="col-md-3 col-8 p-0 d-flex flex-column justify-content-between">
             <div>
                 <h2 class="fs-5"><span class="form-label fs-4">{{__('ui.title')}}</span> {{$article_to_check->title}}</h2>
                 <h3 class="fs-5"> <span class="form-label fs-4">{{__('ui.seller')}}:</span> {{$article_to_check->user->name}}</h3>
